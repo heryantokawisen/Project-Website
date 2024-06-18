addd4463f2f8dc13e64a7a1fecad722367a43545
heryantokawisen <heryantokawisen2003@gmail.com>
2024-06-18 19:05:38 +0700

    WEBSITE TRAVEL

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Travel</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="layar-dalam">
+        <div class="logo">
+          <a href=""><img src="asset/logo-white.png" class="putih" /></a>
+          <a href=""><img src="asset/logo-black.png" class="hitam" /></a>
+        </div>
+        <div class="menu">
+          <a href="#" class="tombol-menu">
+            <span class="garis"></span>
+            <span class="garis"></span>
+            <span class="garis"></span>
+          </a>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#tentangkami">Tentang Kami</a></li>
+            <li><a href="#support">Support</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#tim">Tim</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#kontak">Kontak</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="layar-penuh ">
+      <header id="home">
+        <div class="overlay"></div>
+        <img src="asset/gambar100.jpg" alt="Indonesia Image" style="width: 100%; height:auto; object-fit: cover;" />
+          
+        </div>
+      </header>
+      <main>
+        <section id="tentangkami">
+                  <div class="layar-dalam">
+            <h3>Tentang Kami</h3>
+            <p class="ringkasan">
+             The mind is like a parachute,it won't work if it's not open 
+            </p>
+            <div class="konten-isi">
+              <p>
+               Sedikit tentang kami
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="abuabu" id="support">
+          <div class="layar-dalam support">
+            <div>
+              <img src="asset/matahari.png" />
+              <h6>Setiap Kondisi</h6>
+              <p>
+              Kami siap melayani Anda dalam kondisi apa pun, kami yang terbaik, dan Anda tidak perlu khawatir tentang apa pun.
+              </p>
+            </div>
+            <div>
+              <img src="asset/tas.png" />
+              <h6>Tim profesional</h6>
+              <p>
+              Kami terdiri dari individu-individu profesional yang dilengkapi dengan sertifikasi profesional, yang telah bekerja di industri ini selama bertahun-tahun
+              </p>
+            </div>
+            <div>
+              <img src="asset/kompas.png" />
+              <h6>Pendaki Profesional</h6>
+              <p>
+              Kami adalah pendaki profesional, dan kami akan membawa Anda menjelajahi setiap sudut pegunungan, membimbing Anda sampai ke puncak. Anda adalah prioritas kami.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section id="gallery">
+          <div><img src="asset/foto1.jpg" /></div>
+          <div><img src="asset/foto2.jpg" /></div>
+          <div><img src="asset/foto3.jpg" /></div>
+          <div><img src="asset/foto4.jpg" /></div>
+          <div><img src="asset/foto5.jpg" /></div>
+          <div><img src="asset/foto6.jpg" /></div>
+          <div><img src="asset/foto7.jpg" /></div>
+          <div><img src="asset/foto8.jpg" /></div>
+        </section>
+        <section class="quote">
+          <div class="layar-dalam">
+            <p>Selalu ada rindu untukmu,terima kasih telah berkunjung</p>
+          </div>
+        </section>
+        <section id="tim">
+          <div class="layar-dalam">
+            <h3>Tim Kami</h3>
+            <p class="ringkasan">
+              Kenali kami lebih dekat
+            </p>
+            <div class="tim">
+            <div>
+                <img src="asset/gambarA.jpg" />
+                <h6>Patrick</h6>
+                <span>Indonesia</span>
+              </div>
+              <div>              
+              <img src="asset/gambarC.jpg" />
+                <h6>Rafael</h6>
+                <span>Indonesia</span>
+            </div>
+            <div>              
+              <img src="asset/gambarE.jpg" />
+                <h6>Melati</h6>
+                <span>Indonesia</span>
+            </div>
+            <div>              
+              <img src="asset/gambarF.jpg" />
+                <h6>Alpon</h6>
+                <span>Indonesia</span>
+            </div>
+            <div>              
+              <img src="asset/gambarD.jpg" />
+                <h6>Kawisen</h6>
+                <span>Indonesia</span>
+            </div>
+            <div>
+                <img src="asset/gambarB.jpg" />
+                <h6>William</h6>
+                <span>Indonesia</span>
+              </div>
+          </div>
+        </section>
+        <section class="abuabu" id="blog">
+          <div class="layar-dalam">
+            <h3>Blog Terakhir</h3>
+            <p class="ringkasan">
+              Kembali lah,kami menunggu mu
+            </p>
+            <div class="blog">
+              <div class="area">
+                <div
+                  class="gambar"
+                  style="background-image: url('asset/blog1.jpg')"
+                ></div>
+                <div class="text">
+                  <article>
+                    <h4><a href="#">Bagaimana Dengan Bromo?</a></h4>
+                    <p>
+                    Salah satu daya tarik utama di Gunung Bromo adalah pemandangan matahari terbit dari puncaknya, menciptakan pemandangan yang menakjubkan dengan kabut yang menutupi lautan pasir. Selain itu, Anda dapat mendaki ke puncak Gunung Bromo untuk menyaksikan kawahnya yang aktif.
+                    </p>
+                  </article>
+                </div>
+              </div>
+              <div class="area">
+                <div
+                  class="gambar"
+                  style="background-image: url('asset/blog2.jpg')"
+                ></div>
+                <div class="text">
+                  <article>
+                    <h4><a href="#">Bagaimana Dengan Semeru?</a></h4>
+                    <p>
+                    Gunung Semeru adalah gunung tertinggi di pulau Jawa, Indonesia, dengan ketinggian sekitar 3.676 meter di atas permukaan laut. Gunung ini merupakan bagian dari Taman Nasional Bromo Tengger Semeru dan merupakan tujuan pendakian yang populer. Puncaknya dikenal sebagai Mahameru, yang berarti "Gunung Besar" dalam bahasa Jawa.
+                    </p>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer id="kontak">
+        <div class="layar-dalam">
+          <div>
+            <h5>INFO</h5>
+            Kami menyediakan akomodasi wisata dan perjalanan terbaik untuk Anda, bersama dengan pengalaman yang tak terlupakan. Kami berharap Anda menikmati layanan kami, dan jika ada sesuatu yang ingin Anda tanyakan, jangan ragu untuk menghubungi kami.
+            Salam hangat dari tepi surga.
+          </div>
+          <div>
+            <h5>KONTAK</h5>
+            <a href="https://wa.link/m4ps06">  
+           <img src="https://cdn-icons-png.flaticon.com/128/5968/5968841.png" alt="" height="40">
+           <a href="https://instagram.com/heryanto_kawisen?igshid=YzAwZjE1ZTI0Zg==">
+            <img src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" alt="" height="40">
+            <a href="https://www.tiktok.com/@heryantokawisen?_t=8hzCC2xc7yZ&_r=1">
+              <img src="https://cdn-icons-png.flaticon.com/128/4782/4782345.png" alt="" height="41">
+</a>
+           </div>
+          <div>
+            <h5>ALAMAT</h5>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127661.34840315179!2d112.84867649176722!3d0.8125233153719282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3201c1be0b6c8bd5%3A0xdf0e9576b8ebf8bd!2sKec.%20Putussibau%20Sel.%2C%20Kabupaten%20Kapuas%20Hulu%2C%20Kalimantan%20Barat!5e0!3m2!1sid!2sid!4v1701939561820!5m2!1sid!2sid"
+                  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">class="map</iframe> 
+          </div>
+        </div>
+        <div class="layar-dalam">
+          <div class="copyright">&copy; 2024 VISIT INDONESIA</div>
+        </div>
+      </footer>
+    </div>
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"
+    ></script>
+    <script src="javascript.js"></script>
+  </body>
+</html>
+
+
